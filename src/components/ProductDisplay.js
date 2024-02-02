efce7dbc72da1c87837bff120228deb8765faba0
codegrann <omondihills02@gmail.com>
2024-02-02 16:02:30 +0300

    .

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -1,60 +1,72 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductDisplay.css";
 
+const ourProducts = [
+  {
+    id: 1,
+    name: "Hoodie",
+    price: 10,
+    imageUrl: "hoodie.png",
+    category: "dressing",
+  },
+  {
+    id: 2,
+    name: "T-Shirt",
+    price: 15,
+    imageUrl: "tee.png",
+    category: "dressing",
+  },
+  {
+    id: 3,
+    name: "Short Trouser",
+    price: 10,
+    imageUrl: "short.jpeg",
+    category: "dressing",
+  },
+  {
+    id: 4,
+    name: "Chair",
+    price: 10,
+    imageUrl: "chair.jpeg",
+    category: "furniture",
+  },
+  {
+    id: 5,
+    name: "Bed",
+    price: 10,
+    imageUrl: "bed.jpeg",
+    category: "furniture",
+  },
+  {
+    id: 6,
+    name: "Shelf",
+    price: 10,
+    imageUrl: "shelf.jpeg",
+    category: "furniture",
+  },
+];
 const ProductDisplay = ({ addToCart }) => {
   const [category, setCategory] = useState("all");
-  const products = [
-    {
-      id: 1,
-      name: "Hoodie",
-      price: 10,
-      imageUrl: "hoodie.png",
-      category: "dressing",
-    },
-    {
-      id: 2,
-      name: "T-Shirt",
-      price: 15,
-      imageUrl: "tee.png",
-      category: "dressing",
-    },
-    {
-      id: 3,
-      name: "Short Trouser",
-      price: 10,
-      imageUrl: "short.jpeg",
-      category: "dressing",
-    },
-    {
-      id: 4,
-      name: "Chair",
-      price: 10,
-      imageUrl: "chair.jpeg",
-      category: "furniture",
-    },
-    {
-      id: 5,
-      name: "Bed",
-      price: 10,
-      imageUrl: "bed.jpeg",
-      category: "furniture",
-    },
-    {
-      id: 6,
-      name: "Shelf",
-      price: 10,
-      imageUrl: "shelf.jpeg",
-      category: "furniture",
-    },
-  ];
+  const [products, setProducts] = useState(ourProducts);
+  useEffect(() => {
+    filterProducts();
+  }, [category]);
 
-  if (category === "all") {
-    products.filter((product) => product.category === "all");
-  } else if (category === "dressing") {
-    products.filter((product) => product.category === "dressing");
-  } else if (category === "furniture") {
-    products.filter((product) => product.category === "furniture");
+  function filterProducts() {
+    if (category === "all") {
+      setProducts(products.filter((product) => product.category === "all"));
+    } else if (category === "dressing") {
+      setProducts(
+        products.filter((product) => product.category === "dressing")
+      );
+    } else if (category === "furniture") {
+      setProducts(
+        products.filter((product) => product.category === "furniture")
+      );
+    }
+    return products;
   }
+  // filterProducts();
   // products.filter((product) => product.category === "dressing");
 
   return (
@@ -62,18 +74,19 @@ const ProductDisplay = ({ addToCart }) => {
       <div className="category-filter">
         <label htmlFor="categry">Category:</label>
         <select name="category" id="category">
-          <option value="all" onClick={() => setCategory("all")}>
+          <option value="all" onSelect={() => setCategory("all")}>
             All
           </option>
           <option value="dressing" onSelect={() => setCategory("dressing")}>
             Dressing
           </option>
-          <option value="furniture" onClick={() => setCategory("furniture")}>
+          <option value="furniture" onSelect={() => setCategory("furniture")}>
             Furniture
           </option>
         </select>
       </div>
       <div className="ProductDisplay">
+        {/* {filterProducts()} */}
         {products.map((product) => (
           <div key={product.id} className="ProductItem">
             <img src={product.imageUrl} alt={product.name} />
