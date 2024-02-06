df2188ffc8f7c6a20b6f5e019fa9d75c99e0d103
codegrann <omondihills02@gmail.com>
2024-02-06 04:25:19 +0300

    .

--- README.md
+++ README.md
@@ -6,7 +6,4 @@ footer
 user profile
 invoice
 
-user image
-paypal button
 check if logged in to show profile icon
-hide paypal api key
--- src/components/ConfirmationModal.js
+++ src/components/ConfirmationModal.js
@@ -1,6 +1,4 @@
 import React from "react";
-
-import PaypalButton from "./PaypalButton";
 import "./Modal.css";
 
 const ConfirmationModal = ({ closeModal, cartItems }) => {
@@ -9,7 +7,6 @@ const ConfirmationModal = ({ closeModal, cartItems }) => {
     <div className="ModalOverlay">
       <div className="ConfirmationModal">
         <p>Order submitted. Thank you for shopping with us!</p>
-        <PaypalButton />
         <button onClick={closeModal}>OK</button>
       </div>
     </div>
